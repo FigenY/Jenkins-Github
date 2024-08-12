d5e490419ea0d59c23ecdbd5f0a41e482864550c
FigenY <162337047+FigenY@users.noreply.github.com>
2024-08-12 10:54:09 +0200

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Commit') {
+            steps {
+                echo 'Code committed'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Building the Code...'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Regression tests running...'
+                sh 'javac Hello.java'
+                sh 'java Hello'
+            }
+        }
+        stage('Report') {
+            steps {
+                echo 'Reporting results...'
+            }
+        }
+    }
+}
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+    }
+}
